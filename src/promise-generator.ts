677164e51106b0227012da64360ec28ffe673a93
Otto Kartye <ottokartye@gmail.com>
2017-02-06 17:11:54 +0100

    changed name for more clarity

--- src/promise-generator.ts
+++ src/promise-generator.ts
@@ -1,10 +1,11 @@
 /**
- * Returns a promise that will randomly 
+ * Returns a promise that will randomly
  * resolve a number or reject with false.
  * 
- * @returns
+ * @export
+ * @returns {Promise<any>}
  */
-export function randomPromise() {
+export function randomPromise(): Promise<any> {
   return new Promise((resolve, reject) => {
     const timeOut = setTimeout(() => {
       let number = Math.random();
@@ -20,11 +21,13 @@ export function randomPromise() {
 
 /**
  * Returns a simple promise.
+ * It will resolve if you pass true or fail if you pass false.
  * 
- * @param {any} shouldResolve
- * @returns true or false
+ * @export
+ * @param {boolean} [shouldResolve=true]
+ * @returns {Promise<any>}
  */
-export function returnAPromise(shouldResolve) {
+export function returnAPromise(shouldResolve: boolean = true): Promise<any> {
   return new Promise((resolve, reject) => {
     if (shouldResolve) {
       resolve(true);
