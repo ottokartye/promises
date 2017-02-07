8040bf54c75f24841622d96ccfb9961b36659046
Otto Kartye <ottokartye@gmail.com>
2017-02-07 11:36:50 +0100

    exported raceResolved function and removed in-file testing

--- src/race-resolved.ts
+++ src/race-resolved.ts
@@ -1,4 +1,4 @@
-import { randomPromise } from './promise-generator';
+import { any } from './any';
 
 /**
  * Resolve the first resolved promise.
@@ -6,39 +6,12 @@ import { randomPromise } from './promise-generator';
  * @param {((Promise<any> | any)[])} promises
  * @returns {Promise<any>}
  */
-function raceResolved(promises: (Promise<any> | any)[]): Promise<any> {
+export function raceResolved(promises: Promise<any>[]): Promise<any> {
   return new Promise((resolve, reject) => {
-    let result: any = undefined;
-
-    let numberOfPromises = promises.length;
     promises.forEach((currentPromise, index) => {
-
-      const storeValue = (value) => {
-        result = {
-          index,
-          value
-        };
-        if (index >= numberOfPromises - 1) {
-          resolve(result);
-        }
-      }
-
-      if (!(currentPromise instanceof Promise)) {
-        storeValue(currentPromise);
-      }
-
-      currentPromise
-        .then(storeValue, error => { });
+      currentPromise.then(resolve, () => {});
     });
+
+    any(promises).then(() => reject('No resolved promise found'));
   });
 }
-
-const promise0 = randomPromise();
-const promise1 = randomPromise();
-const promise2 = randomPromise();
-const promise3 = randomPromise();
-
-raceResolved([promise0, promise1, promise2, promise3])
-  .then(result => {
-    console.log('Resolved promise', result);
-  });
