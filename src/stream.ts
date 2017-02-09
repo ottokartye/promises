8e7b6d9219a0f70334b5c97c3dfb807809c48ac1
Otto Kartye <ottokartye@gmail.com>
2017-02-09 16:25:28 +0100

    changed logic for stream method

--- src/stream.ts
+++ src/stream.ts
@@ -1,43 +1,47 @@
-/**
- * Creates an array and marks 
- * each element as undefined.
- * 
- * @param {number} max
- * @returns
- */
-function initValues(max: number) {
-  const values: any[] = [];
-  for (let i = 0; i < max; i++) {
-    values[i] = undefined;
-  }
-  return values;
-}
+export type TProgressCallback = (values: any[], newIndex: number) => void;
+export type TExtendedPromise = Promise<any[]> & {progress?: (callback: TProgressCallback) => TExtendedPromise};
 
 /**
- * Resolve each passed promise and execute the passed 
+ * Resolve each passed promise and execute the passed
  * callback after each resolve/rejected promise.
- * 
+ *
  * @param {((Promise<any> | any)[])} promises
  * @param {any} progressCallback
  * @returns {Promise<any>}
  */
-export function stream(promises: (Promise<any> | any)[], progressCallback): Promise<any[]> {
-  return new Promise(resolve => {
-    const max = promises.length;
-    // Mark all elements of the array as undefined
-    const values: any = initValues(max);
+export function stream(promises: (Promise<any> | any)[]): TExtendedPromise {
+  const progressCallback: TProgressCallback[] = [];
+  const promise: TExtendedPromise = new Promise(resolve => {
+
+    const values: any[] = [];
+    let max = promises.length;
 
     promises.forEach((currentPromise: Promise<any> | any, index: number) => {
-      const store = (value) => {
+
+      const addValue = (value) => {
         values[index] = value;
-        if (index >= max - 1) {
+        progressCallback.forEach((callback) => {
+          callback(values, index);
+        });
+
+        if (--max <= 0) {
           resolve(values);
         }
-        progressCallback(values, index);
       };
 
-      currentPromise
-        .then(store, store);
+      if (!(currentPromise instanceof Promise)) {
+        addValue(currentPromise);
+      }
+
+      currentPromise.then(addValue, addValue);
     });
+
   });
+
+  promise.progress = (callback: TProgressCallback) => {
+    progressCallback.push(callback);
+    return promise;
+  };
+
+  return promise;
 }
