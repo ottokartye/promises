e907d9bb3c560c365f8362b35c8d344109f1ddcd
Otto Kartye <ottokartye@gmail.com>
2017-02-09 16:28:56 +0100

    updated test or stream method

--- src/test/stream.test.ts
+++ src/test/stream.test.ts
@@ -5,21 +5,26 @@ import { expect } from 'chai';
 describe('Stream method', () => {
 
   it('should return [false, 10, false]', async () => {
+    const indexCollection: number[] = [];
     const promiseCollection = [returnAPromise(false), returnAPromise(10), returnAPromise(false)];
-    const progressCallback = (values, index) => {
-      // console.log(index, ': ', values);
-    };
-    const result = await stream(promiseCollection, progressCallback).catch(error => false);
+    const result = await stream(promiseCollection)
+                          .progress((values, index) => {
+                            indexCollection.push(index);
+                          })
+                          .catch(error => false);
     expect(result).to.eql([false, 10, false]);
+    expect(indexCollection).to.eql([1, 0, 2]);
   });
 
   it('should return [false, false, false]', async () => {
+    const indexCollection: number[] = [];
     const promiseCollection = [returnAPromise(false), returnAPromise(false), returnAPromise(false)];
-    const progressCallback = (values, index) => {
-      // console.log(index, ': ', values);
-    };
-    const result = await stream(promiseCollection, progressCallback).catch(error => false);
+    const result = await stream(promiseCollection)
+                          .progress((values, index) => {
+                            indexCollection.push(index);
+                          })
+                          .catch(error => false);
     expect(result).to.eql([false, false, false]);
+    expect(indexCollection).to.eql([0, 1, 2]);
   });
-
 });
